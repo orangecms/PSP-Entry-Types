75cf4b78776072247e65f792c8bd9586f7274cc6
Daniel Maslowski <info@orangecms.org>
2020-10-18 17:59:42 +0200

    format pspEntries.py

--- pspEntries.py
+++ pspEntries.py
@@ -7,25 +7,25 @@ from pathlib import PurePath, Path
 
 # csv file nam
 def getEntries():
-	p = PurePath(__file__)
-	filename = os.path.join(p.parent, "types.csv")
-	
-	f = open(filename, 'r')
-	
-	DIRECTORY_ENTRY_TYPES = {}
-	
-	with f:
-    	
-	    reader = csv.DictReader(f)
-	    
-	    for row in reader:
-	        id = row['ID']
-	        name = row['Name']
-	        pName = row['ProposedName']
-	        comment = row['Comment']
-	        DIRECTORY_ENTRY_TYPES[int(id, 0)] = {}
-	        DIRECTORY_ENTRY_TYPES[int(id, 0)]['Name'] = name
-	        DIRECTORY_ENTRY_TYPES[int(id, 0)]['PName'] = pName
-	        DIRECTORY_ENTRY_TYPES[int(id, 0)]['Comment'] = comment
-	
-	return DIRECTORY_ENTRY_TYPES
+    p = PurePath(__file__)
+    filename = os.path.join(p.parent, "types.csv")
+
+    f = open(filename, 'r')
+
+    DIRECTORY_ENTRY_TYPES = {}
+
+    with f:
+
+        reader = csv.DictReader(f)
+
+        for row in reader:
+            id = row['ID']
+            name = row['Name']
+            pName = row['ProposedName']
+            comment = row['Comment']
+            DIRECTORY_ENTRY_TYPES[int(id, 0)] = {}
+            DIRECTORY_ENTRY_TYPES[int(id, 0)]['Name'] = name
+            DIRECTORY_ENTRY_TYPES[int(id, 0)]['PName'] = pName
+            DIRECTORY_ENTRY_TYPES[int(id, 0)]['Comment'] = comment
+
+    return DIRECTORY_ENTRY_TYPES
